f8a13005cd103e54ca229ac01398e8851a98be4e
Krzychu <skrzychuz@wp.pl>
2020-09-07 22:22:47 +0200

    initial commit

---
+++ src/main/java/com/szponka/loganaliser/LoganaliserApplication.java
@@ -0,0 +1,13 @@
+package com.szponka.loganaliser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoganaliserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoganaliserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szponka/loganaliser/LoganaliserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szponka.loganaliser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoganaliserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
